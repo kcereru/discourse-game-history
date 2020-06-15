025d1e63684c2de6383c4517bce3af45eaa4f06a
KC Maddever <kcereru@gmail.com>
2020-06-15 19:30:58 +0800

    feat: save alive tags in first post to topic custom fields
    Saved as 'players'

--- plugin.rb
+++ plugin.rb
@@ -3,8 +3,8 @@
 # name: discourse-game-history
 # about: A Discourse plugin for Mafia451 that allows users to filter completed games by various game features.
 # version: 0.1
-# authors: kcereru
-# url: https://github.com/kcereru
+# authors: KC Maddever (kcereru)
+# url: https://github.com/kcereru/discourse-game-history
 
 register_asset 'stylesheets/common/discourse-game-history.scss'
 register_asset 'stylesheets/desktop/discourse-game-history.scss', :desktop
@@ -18,4 +18,34 @@ load File.expand_path('lib/discourse-game-history/engine.rb', __dir__)
 
 after_initialize do
   # https://github.com/discourse/discourse/blob/master/lib/plugin/instance.rb
+  on(:post_edited) do |post|
+    if post.is_first_post?
+
+      html  = post.cooked
+      doc   = Nokogiri::HTML.parse(html)
+
+      alive_elements  = doc.xpath("//div[@class='alive']")
+
+      if(alive_elements.last)
+
+        stripped  = ActionController::Base.helpers.strip_tags(alive_elements.last.text)
+        usernames = stripped.split("\n")
+
+        # clean + remove empty lines
+
+        usernames.map! {|player| player.tr('@', '')}
+        usernames.reject!(&:blank?)
+
+        # convert to ids, ignore invalid users
+
+        players     = usernames.map { |username| User.find_by_username_lower(username.downcase) }.compact
+        player_ids  = players.map { |user| user.id }
+
+        # save
+
+        post.topic.custom_fields['players'] = player_ids.join(',')
+        post.topic.save_custom_fields(true)
+      end
+    end
+  end
 end
